c3ff6d7160704079137fbefff668a7db9513d276
Shalane Proctor <shalane088@gmail.com>
2022-07-11 21:45:49 -0500

    started generating joke and changing button on click

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,22 @@
+import { useState } from 'react';
+import getJoke from '../api/jokeData';
+
 function Home() {
+  const [joke, setJoke] = useState({});
+  const [buttonText, buttonTextValue] = useState({ text: 'Get a Joke!' });
+
+  const handleClick = () => {
+    getJoke().then(setJoke);
+    buttonTextValue((prevState) => prevState === { text: 'Get Punchline!' });
+    return (
+      <>
+        <h2>{joke.setup}</h2>
+      </>
+    );
+  };
+
+  // States Get a Joke (button only), render joke and update button to "Get punchline", show punchline"
+
   return (
     <div
       className="text-center d-flex flex-column justify-content-center align-content-center"
@@ -9,7 +27,10 @@ function Home() {
         margin: '0 auto',
       }}
     >
-      <h1>Welcome Home!</h1>
+      <h2>{joke.delivery}</h2>
+      <button type="button" onClick={handleClick}>
+        {buttonText.text}
+      </button>
     </div>
   );
 }
