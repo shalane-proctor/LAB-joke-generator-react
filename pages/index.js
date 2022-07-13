e804a820d7ff32ffb7bfb51319a739ef11b295ff
Shalane Proctor <shalane088@gmail.com>
2022-07-12 20:28:01 -0500

    Button functions and changes. Punchline and setup show up when prompted.

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,17 @@ import getJoke from '../api/jokeData';
 
 function Home() {
   const [joke, setJoke] = useState({});
-  const [buttonText, buttonTextValue] = useState({ text: 'Get a Joke!' });
+  const [buttonText, buttonTextValue] = useState('Get a joke!');
 
+  const currentButtonText = (text) => { buttonTextValue(text); };
   const handleClick = () => {
-    getJoke().then(setJoke);
-    buttonTextValue((prevState) => prevState === { text: 'Get Punchline!' });
-    return (
-      <>
-        <h2>{joke.setup}</h2>
-      </>
-    );
+    getJoke().then((text) => {
+      setJoke({
+        setup: text.setup,
+        delivery: text.delivery,
+      });
+      currentButtonText('Get punchline!');
+    });
   };
 
   // States Get a Joke (button only), render joke and update button to "Get punchline", show punchline"
@@ -27,10 +28,21 @@ function Home() {
         margin: '0 auto',
       }}
     >
-      <h2>{joke.delivery}</h2>
-      <button type="button" onClick={handleClick}>
-        {buttonText.text}
-      </button>
+      <>
+        <h2>{joke.setup}</h2>
+        <h5>{buttonText === 'Get punchline!' ? joke.delivery : undefined}</h5>
+        <div>
+          {buttonText === 'Get a joke!' || buttonText === 'Get a new joke?' ? (
+            <button type="button" onClick={handleClick}>
+              {buttonText}
+            </button>
+          ) : (
+            <button type="button" onClick={() => currentButtonText('Get a new joke?')}>
+              {buttonText}
+            </button>
+          )}
+        </div>
+      </>
     </div>
   );
 }
